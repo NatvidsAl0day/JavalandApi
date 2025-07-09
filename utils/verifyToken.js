5362ee6ed200ea5650a9f2d89c007d32edcba4de
Natvids Al0day <168284843+NatvidsAl0day@users.noreply.github.com>
2025-07-09 18:33:05 +0700

    Update verifyToken.js

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,59 +1,42 @@
+// utils/verifyToken.js
 import jwt from 'jsonwebtoken';
 
-const verifyToken = (req, res, next) => {
-    const token = req.cookies.accessToken;
-
-    if (!token) {
-        return res.status(401).json({
-            success: false,
-            message: 'You Not Authorize'
-        })
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.accessToken;
+  if (!token) {
+    return res.status(401).json({ success: false, message: 'You Not Authorize' });
+  }
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(401).json({ success: false, message: 'Expired TOKEN' });
     }
+    req.user = user;
+    next();
+  });
+};
 
-    // if token expired
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
-        if (err) {
-            return res.status(401).json({
-                success:false,
-                message: 'Expired TOKEN'
-            })
-        }
+// Middleware untuk memeriksa user atau admin
+export const verifyUser = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.role === 'admin') {
+      return next();
+    }
+    return res.status(403).json({ success: false, message: 'Bukan Autoritas' });
+  });
+};
 
-        req.user = user
-        next()
-    })
-
-
-}
-
-export const verifyUSer = (req, res, next) => {
-    verifyToken(req, res, next, () => {
-        // if (req.user.id === req.params.id || req.user.role === 'admin') {
-        if (req.user.id === req.params.id || req.user.role === 'admin') {
-            next();
-        } else {
-            return res.status(401).json({
-                success:false,
-                message: 'Bukan Autoritas'
-            })
-
-        }
-    })
-}
-
+// Middleware untuk memeriksa hanya admin
 export const verifyAdmin = (req, res, next) => {
-    verifyToken(req, res, next, () => {
-        if (req.user.role === "admin") {
-            next();
-        } else {
-            return res.status(401).json({
-                success:false,
-                message: 'Bukan Autoritas'
-            })
+  verifyToken(req, res, () => {
+    if (req.user.role === 'admin') {
+      return next();
+    }
+    return res.status(403).json({ success: false, message: 'Bukan Autoritas' });
+  });
+};
 
-        }
-    })
-}
+
+
 
 // import jwt from 'jsonwebtoken';
 
@@ -63,45 +46,102 @@ export const verifyAdmin = (req, res, next) => {
 //     if (!token) {
 //         return res.status(401).json({
 //             success: false,
-//             message: 'You are not authorized'
-//         });
+//             message: 'You Not Authorize'
+//         })
 //     }
 
-//     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+//     // if token expired
+//     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
 //         if (err) {
 //             return res.status(401).json({
-//                 success: false,
-//                 message: 'Expired or invalid token'
-//             });
+//                 success:false,
+//                 message: 'Expired TOKEN'
+//             })
 //         }
-        
-//         req.user = decoded;
-//         next();
-//     });
-// };
+
+//         req.user = user
+//         next()
+//     })
+
+
+// }
 
 // export const verifyUSer = (req, res, next) => {
-//     verifyToken(req, res, () => {
+//     verifyToken(req, res, next, () => {
+//         // if (req.user.id === req.params.id || req.user.role === 'admin') {
 //         if (req.user.id === req.params.id || req.user.role === 'admin') {
 //             next();
 //         } else {
 //             return res.status(401).json({
-//                 success: false,
-//                 message: 'Not authorized'
-//             });
+//                 success:false,
+//                 message: 'Bukan Autoritas'
+//             })
+
 //         }
-//     });
-// };
+//     })
+// }
 
 // export const verifyAdmin = (req, res, next) => {
-//     verifyToken(req, res, () => {
-//         if (req.user.role === 'admin') {
+//     verifyToken(req, res, next, () => {
+//         if (req.user.role === "admin") {
 //             next();
 //         } else {
 //             return res.status(401).json({
-//                 success: false,
-//                 message: 'Not authorized'
-//             });
+//                 success:false,
+//                 message: 'Bukan Autoritas'
+//             })
+
 //         }
-//     });
-// };
+//     })
+// }
+
+// // import jwt from 'jsonwebtoken';
+
+// // const verifyToken = (req, res, next) => {
+// //     const token = req.cookies.accessToken;
+
+// //     if (!token) {
+// //         return res.status(401).json({
+// //             success: false,
+// //             message: 'You are not authorized'
+// //         });
+// //     }
+
+// //     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+// //         if (err) {
+// //             return res.status(401).json({
+// //                 success: false,
+// //                 message: 'Expired or invalid token'
+// //             });
+// //         }
+        
+// //         req.user = decoded;
+// //         next();
+// //     });
+// // };
+
+// // export const verifyUSer = (req, res, next) => {
+// //     verifyToken(req, res, () => {
+// //         if (req.user.id === req.params.id || req.user.role === 'admin') {
+// //             next();
+// //         } else {
+// //             return res.status(401).json({
+// //                 success: false,
+// //                 message: 'Not authorized'
+// //             });
+// //         }
+// //     });
+// // };
+
+// // export const verifyAdmin = (req, res, next) => {
+// //     verifyToken(req, res, () => {
+// //         if (req.user.role === 'admin') {
+// //             next();
+// //         } else {
+// //             return res.status(401).json({
+// //                 success: false,
+// //                 message: 'Not authorized'
+// //             });
+// //         }
+// //     });
+// // };
