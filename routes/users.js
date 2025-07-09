cf09fdd7fc3024a2ce133ef9c001ce67835e4a0b
Natvids Al0day <168284843+NatvidsAl0day@users.noreply.github.com>
2025-07-09 20:09:15 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -21,13 +21,13 @@ console.log('Current working directory:', process.cwd());
 console.log('Path to userController:', require.resolve('../controller/userController.js'));
 
 // Update User (user sendiri atau admin)
-router.put('/:id', verifyUser, updateUser);
+router.put('/:id', verifyUSer, updateUser);
 
 // Delete User (user sendiri atau admin)
-router.delete('/:id', verifyUser, deleteUser);
+router.delete('/:id', verifyUSer, deleteUser);
 
 // Get Single User (user sendiri atau admin)
-router.get('/:id', verifyUser, getSingleUser);
+router.get('/:id', verifyUSer, getSingleUser);
 
 // Get All Users (hanya admin)
 router.get('/', verifyAdmin, getAllUser);
