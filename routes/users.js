7f09a16d6e8012373093e5448ac30d9f550c4d19
Natvids Al0day <168284843+NatvidsAl0day@users.noreply.github.com>
2025-07-09 19:08:45 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,12 @@ import {
   getAllUser
 } from '../controller/userController.js';    // pastikan ejaannya benar
 import { verifyUser, verifyAdmin } from '../utils/verifyToken.js';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log('users.js running at:', __dirname);
+console.log('looking for controller at:', resolve(__dirname, '../controller/userController.js'));
+
 
 const router = express.Router();
 
