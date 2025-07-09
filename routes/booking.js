965903a4ce6759db57f08322ce50915a7c4dcc1b
Natvids Al0day <168284843+NatvidsAl0day@users.noreply.github.com>
2025-07-09 21:21:29 +0700

    Update booking.js

--- routes/booking.js
+++ routes/booking.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { verifyAdmin, verifyUSer } from '../utils/verifyToken.js';
+import { verifyAdmin, verifyUSer, verifyToken } from '../utils/verifyToken.js';
 import { createBooking, getAllBooking, getBooking } from '../controller/bookingController.js';
 
 
 const router = express.Router()
 
-router.post('/', verifyUSer, createBooking)
+router.post('/', verifyToken, createBooking)
 router.get('/:id', verifyUSer, getBooking)
 router.get('/', verifyAdmin, getAllBooking)
 
